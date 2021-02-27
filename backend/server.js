a660a2e7c190aa04f98196994f4e8c437c24e64d
Yehia Seif <yehia_seif@live.com>
2021-02-27 15:29:34 +0100

    adjust screenshots path in the server

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.get('/api/config/paypal', (req, res) =>
 )
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/screenshots')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
